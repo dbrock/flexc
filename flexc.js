1b373c48c825bb051f83bc813e61bd946365f4f3
Daniel Brockman <daniel@brockman.se>
2012-05-08 04:40:40 +0200

    Initial import.

---
+++ flexc.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+var format = require("util").format
+var inspect = require("util").inspect
+var path = require("path")
+
+var options = require("optimist")
+  .boolean("n", "dry-run")
+  .string("o", "output")
+  .argv
+
+var config = {}
+
+if (options._.length === 0) {
+  die("nothing to compile")
+} else if (options._.length === 1) {
+  config.source_file = options._[0]
+} else {
+  die("error: multiple source files given")
+}
+
+var command
+var file_args = []
+var option_args = []
+
+if (path.existsSync(config.source_file)) {
+  command = "mxmlc"
+  file_args.push(config.source_file)
+  option_args.push("-output=" + (
+    path.resolve(config.source_file).replace(/\.(as|mxml)$/, ".swf")
+  ))
+} else {
+  die("%s: no such file or directory", config.source_file)
+}
+
+file_args.sort()
+option_args.sort()
+
+if (options["n"] || options["dry-run"]) {
+  console.log([command].concat(file_args, option_args).join(" "))
+} else {
+  // XXX
+}
+
+function die() {
+  console.error("flexc: %s", format.apply(null, arguments))
+  process.exit(1)
+}
---
+++ test/test.js
@@ -0,0 +1,78 @@
+#!/usr/bin/env node
+
+var colors = require("colors")
+var exec = require("child_process").exec
+var inspect = require("util").inspect
+var path = require("path")
+
+var n_tests = 0
+
+process.chdir(__dirname)
+
+require("libxmljs").parseXmlString(
+  require("fs").readFileSync(
+    __dirname + "/../flexc-test-cases.html"
+  ).toString()
+).find("//dt").forEach(function (dt, test_index) {
+  var command = dt.text().replace(/^flexc\b/, __dirname + "/../flexc.js")
+  var expected = {
+    stdout: parse_dd(dt.get("following-sibling::*[" + (
+      "position()<=2 and name()='dd' and (@class='stdout' or not(@class))"
+    ) + "]")),
+    stderr: parse_dd(dt.get("following-sibling::*[" + (
+      "position()<=2 and name()='dd' and @class='stderr'"
+    ) + "]"))
+  }
+
+  exec(command, function (error, stdout, stderr) {
+    if (stdout === expected.stdout && stderr === expected.stderr) {
+      ++n_tests
+    } else {
+      dump("Test #" + (test_index + 1) + " failed:", command)
+      dump("Expected stdout:", expected.stdout)
+      dump("Actual stdout:", stdout)
+      dump("Expected stderr:", expected.stderr)
+      dump("Actual stderr:", stderr)
+      n_tests = null
+      process.exit(1)
+    }
+  })
+})
+
+function parse_dd(dd) {
+  var result
+
+  if (!dd) {
+    result = ""
+  } else if (dd.get("div")) {
+    result = dd.find("div").map(function (div) {
+      return div.text() + "\n"
+    }).join("")
+  } else {
+    result = dd.text() + "\n"
+  }
+
+  return result.replace(/\$PWD\b/g, path.resolve(__dirname))
+}
+
+function dump(label, text) {
+  console.error(colors.bold(label))
+
+  if (text.indexOf("\n") >= 0) {
+    console.error(
+      text.replace(/\n$/, "").split("\n").map(function (line, index) {
+        return colors.white((index + 1) + " ┃ ") + line
+      }).join("\n")
+    )
+  } else if (text) {
+    console.error("  " + text)
+  } else {
+    console.error(colors.grey("  (empty)"))
+  }
+}
+
+process.on("exit", function () {
+  if (n_tests !== null) {
+    console.log("OK (ran %d tests).", n_tests)
+  }
+})
